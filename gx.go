6273b13ae395d7d222db14c93afaff10069d4dde
Marco Caggiano <mcisback@gmail.com>
2025-08-31 00:41:15 +0200

    Initial commit

---
+++ gx.go
@@ -0,0 +1,62 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"regexp"
+)
+
+func main() {
+	if len(os.Args) < 2 {
+		fmt.Fprintf(os.Stderr, "Usage:\n\t%s <regex> [<optional_replacement>]\n", os.Args[0])
+		os.Exit(1)
+	}
+
+	pattern := os.Args[1]
+
+	if len(os.Args) == 3 {
+		replacement := os.Args[2]
+
+		re, err := regexp.Compile(pattern)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "Invalid regex: %v\n", err)
+			os.Exit(1)
+		}
+
+		scanner := bufio.NewScanner(os.Stdin)
+		for scanner.Scan() {
+			line := scanner.Text()
+			
+			if re.MatchString(line) {
+				result := re.ReplaceAllString(line, replacement)
+				fmt.Println(result)
+			}
+		}
+
+		if err := scanner.Err(); err != nil {
+			fmt.Fprintf(os.Stderr, "Error reading stdin: %v\n", err)
+			os.Exit(1)
+		}
+	} else {
+		re, err := regexp.Compile(pattern)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "Invalid regex: %v\n", err)
+			os.Exit(1)
+		}
+
+		scanner := bufio.NewScanner(os.Stdin)
+		for scanner.Scan() {
+			line := scanner.Text()
+			if re.MatchString(line) {
+				fmt.Println(line)
+			}
+		}
+
+		if err := scanner.Err(); err != nil {
+			fmt.Fprintf(os.Stderr, "Error reading stdin: %v\n", err)
+			os.Exit(1)
+		}
+	}
+
+}
